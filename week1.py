aa83f4e3255439cdf4ee2aa210168a85dc18f740
Sail338 <srihari.shankar1@gmail.com>
2019-02-21 14:20:26 -0500

    skrt skrt

--- week1.py
+++ week1.py
@@ -1,27 +1,30 @@
 import discord
 import asyncio
 
-client = discord.Client()
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('!test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+        if message.content.startswith('!test'):
+            counter = 0
+            tmp = await message.channel.send('Calculating messages...')
+            async for msg in message.channel.history(limit=100):
+                if msg.author == message.author:
+                    counter += 1
 
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
+            await tmp.edit(content='You have {} messages.'.format(counter))
+        elif message.content.startswith('!sleep'):
+            with message.channel.typing():
+                await asyncio.sleep(5.0)
+                await message.channel.send('Done sleeping.')
 
+client = MyClient()
 client.run('token')
